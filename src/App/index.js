b400b976646e1c60612de26bd4c7103d9c72529f
Francisco Bourquin <122411274+FranciscoBourquin@users.noreply.github.com>
2024-07-16 17:36:52 -0300

    Reestructuración de la app
    Se elimina el contexto, se renombra  TodoCobtext a useTodos y se mueve junto con useLocalStorage a  la carpeta App

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,80 @@
-import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
-
+import { useTodos } from './useTodos';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { TodoLoader } from '../TodoLoader';
+import { TodosError } from '../TodosError';
+import { EmptyTodos } from '../EmptyTodos';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from '../Modal';
+import { TodoForm } from '../TodoForm';
 function App() {
+
+  const
+  {
+    loading,
+    completedTodos,
+    totalTodos,
+    error,
+    searchValue,
+    setSearchValue,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    addTodo
+
+ } = useTodos()
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <>
+    <TodoCounter
+      completedTodos = {completedTodos}
+      totalTodos = {totalTodos}
+    />
+    <TodoSearch
+      searchValue = {searchValue}
+      setSearchValue = {setSearchValue}
+    />
+
+        <TodoList>
+          {loading && (
+            <>
+              <TodoLoader />
+            </>
+          )}
+          {error && <TodosError/>}
+          {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
+
+          {searchedTodos.map(todo => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
+          ))}
+        </TodoList>
+
+
+    <CreateTodoButton
+      setOpenModal={setOpenModal}
+     />
+
+    {
+    openModal&& (
+      <Modal>
+          <TodoForm
+            addTodo = {addTodo}
+            setOpenModal={setOpenModal}
+          />
+    </Modal>
+    )
+    }
+  </>
   );
 }
 
